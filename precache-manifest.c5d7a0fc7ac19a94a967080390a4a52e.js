05e6d486a3a9ea0519b222ffde49cd9a81d31c8c
amie-affection <amie.affection@gmail.com>
2020-06-23 19:17:47 +0300

    Updates

---
+++ precache-manifest.c5d7a0fc7ac19a94a967080390a4a52e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a412f71d8764c65a1bd90e64a4819cb6",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "d00abf2e378882d66718",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.459171e1.chunk.css"
+  },
+  {
+    "revision": "a4ad6ceb09652dcb8760",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.a3874bab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.a3874bab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d00abf2e378882d66718",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.29ca1180.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
